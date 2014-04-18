a610cc5be6c395487cc76e89abdcd57d7e3ab0c0
supermomonga <hi@supermomonga.com>
2014-04-18 14:38:39 +0900

    singleton

--- server.rb
+++ server.rb
@@ -24,46 +24,46 @@ end
 
 using StringExtension
 
-class Library
-  @@data = nil
-  def self.data
-    @@data ||= Plist::parse_xml('./example.plist')
+class << Library = Class.new
+  @data = nil
+  def data
+    @data ||= Plist::parse_xml('./example.plist')
   end
-  def self.rows_of name, conditions = {}
-    self.data[name].select{|row|
+  def rows_of name, conditions = {}
+    data[name].select{|row|
       conditions.all? {|property, value|
         property = property.to_s.camelize
         row[property].to_s.include? value
       }
     }
   end
-  def self.track_field_values(field)
-    self.tracks.map{|id,track| track[field] }.uniq.reject(&:nil?)
+  def track_field_values(field)
+    tracks.map{|id,track| track[field] }.uniq.reject(&:nil?)
   end
-  def self.search_tracks_by(filed, value)
-    self.tracks.select{|id,track|
-      self.track_fields.any? {|field| track[field].to_s.include? value }
+  def search_tracks_by(filed, value)
+    tracks.select{|id,track|
+      track_fields.any? {|field| track[field].to_s.include? value }
     }
   end
-  def self.method_missing(name, *args)
+  def method_missing(name, *args)
     name.to_s.tap do |name|
-      if name.plural? && self.data.keys.include?(name.camelize)
-        break self.send(:rows_of, name.camelize, args.first.tap{|_| break {} unless _ })
-      elsif name.plural? && self.track_fields.include?(name.camelize.singularize)
-        break self.send(:track_field_values, name.camelize.singularize)
+      if name.plural? && data.keys.include?(name.camelize)
+        break send(:rows_of, name.camelize, args.first.tap{|_| break {} unless _ })
+      elsif name.plural? && track_fields.include?(name.camelize.singularize)
+        break send(:track_field_values, name.camelize.singularize)
       elsif name.singular? && name.start_with?('search_tracks_by_')
-        break self.send(:search_tracks_by, name.sub('search_tracks_by_', '').camelize, args.first)
+        break send(:search_tracks_by, name.sub('search_tracks_by_', '').camelize, args.first)
       else
         break super
       end
     end
   end
-  def self.track_fields
+  def track_fields
     ['Name','Artist','Album Artist','Composer','Album','Genre','Kind']
   end
-  def self.search_tracks(word)
-    self.tracks.select{|id,track|
-      self.track_fields.any? {|field| track[field].to_s.include? word }
+  def search_tracks(word)
+    tracks.select{|id,track|
+      track_fields.any? {|field| track[field].to_s.include? word }
     }
   end
 end
